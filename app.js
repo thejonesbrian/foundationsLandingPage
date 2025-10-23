edfd06233b8351085cc89b44ab8b856fc2195ac1
brijones.nz@gmail.com <brijones.nz@gmail.com>
2025-10-23 15:16:31 +1300

    playing around with javascript in app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let fname = "Jesus";
+let surname = "Christ";
+
+console.log(`Hello....${fname} ${surname}`);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(percentage);
+
+function favAnimal(animal) {
+    return `${animal} is my favourite animal`;
+}
+
+const animal = favAnimal('Monkey');
+console.log(animal);
