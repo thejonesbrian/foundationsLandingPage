f59da982b570ef67ab3e81b2a7af9e582a724f68
brijones.nz@gmail.com <brijones.nz@gmail.com>
2025-10-29 11:36:00 +1300

    javascript function practice / outputs

--- app.js
+++ app.js
@@ -7,11 +7,38 @@ const max = 57;
 const actual = max - 13;
 const percentage = actual / max;
 
-console.log(percentage);
+//console.log(percentage);
 
 function favAnimal(animal) {
     return `${animal} is my favourite animal`;
 }
 
 const animal = favAnimal('Monkey');
-console.log(animal);
+//console.log(animal);
+
+// Functions
+function add7(num) {
+    return num + 7;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+let answer = add7(10);
+console.log(answer);
+
+let product = multiply(3, 2);
+console.log(product);
+
+function capitalize(word) {
+    let lower = word.toLowerCase();
+    let capitalizedWord = lower.charAt(0).toUpperCase() + lower.slice(1);
+    return capitalizedWord;
+}
+console.log(capitalize("bIngO"));
+
+function lastLetter(word) {
+    return word.slice(-1);
+}
+console.log(lastLetter("apple"));
